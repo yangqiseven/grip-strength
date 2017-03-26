6aff777171a1ec852f7a3dac5b80d2165799c44b
Michael Orr <Michaelorr92@gmail.com>
2017-03-26 21:50:24 +0100

    Added adc thread
    Signed-off-by: Michael Orr <Michaelorr92@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <QApplication>
 #include "mainwindow.h"
+#include "ringBuffer.h"
+#include "adcreader.h"
 
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
   QApplication::setGraphicsSystem("raster");
 #endif
+
   QApplication a(argc, argv);
+
   MainWindow w;
   w.show();
+
+  ADCreader adcReader;
+  adcReader.start();
 
   return a.exec();
 }
