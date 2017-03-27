728d682161a24fc2a46fec0720d22944be597c61
Michael Orr <Michaelorr92@gmail.com>
2017-03-27 19:36:04 +0100

    Added legend with real time data
    Signed-off-by: Michael Orr <Michaelorr92@gmail.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,3 @@
-
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <QDebug>
@@ -6,6 +5,7 @@
 #include <QScreen>
 #include <QMessageBox>
 #include <QMetaEnum>
+#include <QString>
 
 MainWindow::MainWindow(QWidget *parent, ring_buffer_t &buffer, QMutex &mutex) :
   QMainWindow(parent),
@@ -22,11 +22,14 @@ MainWindow::MainWindow(QWidget *parent, ring_buffer_t &buffer, QMutex &mutex) :
 
 void MainWindow::grip_strength(QCustomPlot *customPlot)
 {
+  int i = 0;
 
   customPlot->addGraph(); // blue line
   customPlot->graph(0)->setPen(QPen(QColor(40, 110, 255)));
-  //customPlot->addGraph(); // red line
-  //customPlot->graph(1)->setPen(QPen(QColor(255, 110, 40)));
+
+  customPlot->addGraph(); // red line
+  customPlot->graph(1)->setPen(QPen(QColor(255, 110, 40)));
+
 
   QSharedPointer<QCPAxisTickerTime> timeTicker(new QCPAxisTickerTime);
   timeTicker->setTimeFormat("%h:%m:%s");
@@ -35,6 +38,13 @@ void MainWindow::grip_strength(QCustomPlot *customPlot)
   customPlot->yAxis->setRange(0, 130);
   customPlot->xAxis->setLabel("Time (s)");
   customPlot->yAxis->setLabel("Force (kg)");
+
+  customPlot->legend->setVisible(true);
+  QFont legendFont = font();  // start out with MainWindow's font..
+  legendFont.setPointSize(9); // and make a bit smaller for legend
+  customPlot->legend->setFont(legendFont);
+  customPlot->legend->setBrush(QBrush(QColor(255,255,255,230)));
+  customPlot->axisRect()->insetLayout()->setInsetAlignment(0, Qt::AlignTop|Qt::AlignLeft);
 
   // make left and bottom axes transfer their ranges to right and top axes:
   connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
@@ -51,11 +61,18 @@ void MainWindow::realtimeDataSlot()
   static QTime time(QTime::currentTime());
   double key = time.elapsed()/1000.0; // time elapsed since start of demo, in seconds
   uint8_t buf = 0;
+  static uint8_t max = 0;
 
   // add data to lines:
   mutex.lock();
   while(ring_buffer_dequeue(&ring_buffer, &buf) > 0) { // empty buffer
+    if (buf > max){
+        max = buf; // check for max value
+    }
     ui->customPlot->graph(0)->addData(key, buf);
+    ui->customPlot->graph(1)->addData(key, max);
+    ui->customPlot->graph(0)->setName("Current Force: " + QString::number(buf));
+    ui->customPlot->graph(1)->setName("Maximum Force: " + QString::number(max));
   }
   mutex.unlock();
 
