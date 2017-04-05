1b0e0584bd586f40f84f43b787fd93bbbe958ef8
Michael Orr <Michaelorr92@gmail.com>
2017-04-05 19:18:17 +0100

    Updated refresh rate to be around 66 Hz
    Signed-off-by: Michael Orr <Michaelorr92@gmail.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -51,7 +51,7 @@ void MainWindow::grip_strength(QCustomPlot *customPlot)
 
   // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
   connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
-  dataTimer.start(1); // Interval is in ms 0 means to refresh as fast as possible
+  dataTimer.start(15); // Interval is in ms 0 means to refresh as fast as possible
 }
 
 
